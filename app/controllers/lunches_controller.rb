69c0fddfe8e797fe8b7e219f6562a93f1a398995
aosora222 <59185844+aosora222@users.noreply.github.com>
2020-02-24 20:23:23 +0900

    詳細ページ
    完了

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,4 +1,6 @@
 class LunchesController < ApplicationController
+  before_action :set_lunch, only: [:edit, :update, :show]
+
   def index
     @lunches = Lunch.includes(:images).all.limit(3).order(updated_at: :desc)
   end
@@ -17,8 +19,16 @@ class LunchesController < ApplicationController
     end
   end
 
+  def show
+    @images = @lunch.images
+  end
+
   private
   def lunch_params
     params.require(:lunch).permit(:name, :main, :tel, :addless, :parking, :business_day, :business_time, :holiday, :budget, :hp, :remarks, images_attributes: [:src, :_destroy, :id])
+  end
+
+  def set_lunch
+    @lunch = Lunch.find(params[:id])
   end
 end
