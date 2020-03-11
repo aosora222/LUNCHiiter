25e90b41c7c04d9703b90eb70fc357dcfa94d8a9
aosora222 <59185844+aosora222@users.noreply.github.com>
2020-03-11 14:05:09 +0900

    user登録
    ユーザー登録ができる
    ニックネーム表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kane, :last_kane, :prefecture, :city, :birthday, :phone_number, :profile])
+  end
 
   private
 
